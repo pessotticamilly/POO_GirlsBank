c1e2f143c01792177bf201b6c48ad276dbd6d99a
pessotticamilly <camilly_pessotti@estudante.sc.senai.br>
2022-09-01 08:58:13 -0300

    Initial commit

---
+++ src/PessoaFisica.java
@@ -0,0 +1,44 @@
+public class PessoaFisica extends Pessoa {
+    private String cpf, profissao;
+    private double renda;
+
+    public PessoaFisica(String nome, String endereco, String cpf, String profissao, double renda) {
+        super(nome, endereco);
+        this.cpf = cpf;
+        this.profissao = profissao;
+        this.renda = renda;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(double renda) {
+        this.renda = renda;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nCPF: " + cpf +
+                "\nProfissão: " + profissao +
+                "\nRenda: " + renda +
+                "\n- - - - - - - - - - - - - - - - - - - -\n\n";
+    }
+}
---
+++ src/PessoaJuridica.java
@@ -0,0 +1,23 @@
+public class PessoaJuridica extends Pessoa {
+    private String cnpj;
+
+    public PessoaJuridica(String nome, String endereco, String cnpj) {
+        super(nome, endereco);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public String toString() {
+        return  super.toString() +
+                "\nCNPJ: " + cnpj +
+                "\n- - - - - - - - - - - - - - - - - - - -\n\n";
+    }
+}
